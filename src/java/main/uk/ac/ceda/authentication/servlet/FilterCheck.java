fa694577d2430aff573390f438ac44a85f12037d
watucker <william.tucker@stfc.ac.uk>
2017-07-13 15:16:06 +0100

    Added ivy dependencies and ant build file.

--- src/java/main/uk/ac/ceda/authentication/servlet/FilterCheck.java
+++ src/java/main/uk/ac/ceda/authentication/servlet/FilterCheck.java
@@ -21,7 +21,6 @@ public class FilterCheck extends HttpServlet
     public FilterCheck()
     {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     /**
@@ -30,7 +29,6 @@ public class FilterCheck extends HttpServlet
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        // TODO Auto-generated method stub
         response.getWriter().append("Served at: ").append(request.getContextPath());
     }
 
@@ -40,7 +38,6 @@ public class FilterCheck extends HttpServlet
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        // TODO Auto-generated method stub
         doGet(request, response);
     }
 
